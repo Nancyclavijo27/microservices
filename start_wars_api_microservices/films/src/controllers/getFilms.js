6a1d91a69288bb4ad9ce20b26e99be7e6cc94209
Nancy Clavijo Varela <clavijovarela@gmail.com>
2023-05-25 16:34:07 -0500

    fin

--- start_wars_api_microservices/films/src/controllers/getFilms.js
+++ start_wars_api_microservices/films/src/controllers/getFilms.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 module.exports= async(req, res)=>{
     const data = await axios.get("http://database:8004/Film");
     console.log("films", data.data);
-    response(res, 200, films)
+    response(res, 200, data.data)
 }
 
 
--- start_wars_api_microservices/planets/src/controllers/getPlanets.js
+++ start_wars_api_microservices/planets/src/controllers/getPlanets.js
@@ -4,7 +4,7 @@ const {response }=require("../utils")
 module.exports= async(req, res)=>{
     const data = await axios.get("http://database:8004/Planet");
     console.log("planets", data.data);
-    response(res, 200, planets)
+    response(res, 200, data.data)
 }
 
 
